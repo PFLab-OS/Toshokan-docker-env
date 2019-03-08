77b4ff8ad35f0b4a9ad087f978c0077ad0714421
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-03-08 17:35:53 +0900

    remove cache build

--- copy_kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker run -d --rm -it --name toshokan_kernel_tmp livadk/toshokan_kernel_build sh
-docker cp toshokan_kernel_tmp:/usr/src/linux-headers-4.14.34hakase_4.14.34hakase-2_amd64.deb .
-docker cp toshokan_kernel_tmp:/usr/src/linux-libc-dev_4.14.34hakase-2_amd64.deb .
-docker cp toshokan_kernel_tmp:/usr/src/linux-image-4.14.34hakase_4.14.34hakase-2_amd64.deb .
-docker rm -f toshokan_kernel_tmp
